b591c1d49fcb942fabbe04734c2299a34067b8e3
TseganeshM <mogatsega@gmail.com>
2024-10-31 17:51:31 +0300

    initial commit of FrontEnd

---
+++ Evangadi-Forum-FrontEnd/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h3>Evangadi-Forum-FrontEnd</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
